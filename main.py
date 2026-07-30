ac29b6a07f00d8a94976b82424e776de020094d5
Jason Eckardt <jason.eckardttaing@creekenterprise.com>
2026-07-29 23:24:02 -0400

    [WIP] Add guard for missing envs; Add command; Fix syntax errors; Update client init

--- main.py
+++ main.py
@@ -35,24 +35,39 @@ class ServerConfig:
 class ServerEmbed(discord.Embed):
     ## TODO:= Resolve if needed -> **kwargs are for passing additional embed args without needing to enumerate them
     def __init__(self, config: ServerConfig, **kwargs):
-        super().__init__(color=config.color, **kwargs)
+        #super().__init__(color=config.color, **kwargs)
+        super().__init__()
+
         self.set_thumbnail(url=config.embed_icon)
         self.set_image(url=config.embed_image)
+
+        self.add_field(name="Host", value=config.host)
+        self.add_field(name="Port", value=config.port)
+        self.add_field(name="Name", value=config.name)
+
+        self.set_footer(text=f"{config.host}:{config.port}")
 
         if config.protocol == Protocol.A2S:
             query = (config.query_host, config.query_port)
             info = a2s.info(query)
             rules = a2s.rules(query)
             self.title = info.game
-            self.add_field(name="Name", value=config.name) # User enter server name
             self.add_field(name="Players", value=f"{str(info.player_count)}/{str(info.max_players)}")
             self.add_field(name="Game Version", value=info.version)
-            if config.game = "Abiotic Factor":
-                self.add_field(name="Hardcore", value=rules["Hardcore_b"]
-                self.add_field(name="Story Progress", value=rules["StoryProgress_s"]
-            if config.game = "Project Zomboid":
-                self.add_field(name"Mod Count", value=rules["mod_count"]
-                self.add_field(name="Mods", value=rules["mods"]
+            if info.game == "Abiotic Factor":
+                self.add_field(name="Hardcore", value=rules["Hardcore_b"])
+                self.add_field(name="Story Progress", value=rules["StoryProgress_s"])
+            if info.game == "Project Zomboid":
+                self.add_field(name="Mod Count", value=rules["mod_count"])
+                self.add_field(name="Mods", value=rules["mods"])
+        if self.title == None:
+            print("warn: the game title was not fetched")
+            self.title = config.name
+
+    async def send(self, interaction: discord.Interaction, config: ServerConfig):
+        message = await client.channel.send(embed=self)
+        ServerStore.update(config)
+
 
 class ServerStore:
     def __init__(self):
@@ -89,20 +104,58 @@ class ServerStore:
         self.__apply__()
 
 class Client(discord.Client):
-    async def on_ready(self):
-        print(self.user, "is ready!")
+    user: discord.ClientUser
+
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.channel = None
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild=os.getenv("GUILD_ID"))
+        await self.tree.sync(guild=os.getenv("GUILD_ID"))
+        self.channel = await self.fetch_channel(os.getenv("CHANNEL_ID"))
+
 
 intents = discord.Intents.default()
-intents.message_content = True
 client = Client(intents=intents)
-#client.run(os.getenv('BOT_TOKEN'))
 
-servers = ServerStore()
-new_server = ServerConfig("lngs.lambdastack.org", "Abiotic Gamer", 7777, Protocol.A2S, "10.0.3.103", 27015, "#FFFFFF","someiconlol", "some_msg_id_lol", "someimglol")
-servers.update(new_server)
-query = (new_server.query_host, new_server.query_port)
-server_players = a2s.players(query)
-server_info = a2s.info(query)
-server_rules = a2s.rules(query)
 
-print(server_info, server_rules, server_players)
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user} (ID: {client.user.id})")
+    print("------")
+
+@client.tree.command(description="Add a server, opens Add Server modal.")
+@app_commands.describe(
+    server_name="Display name of the server",
+    ip_address="IP Address players can join with.",
+    port="Port number for server",
+    color="Color for embed in HEX format.",
+    icon_url="Icon displayed in the embed footer",
+    image_url="Image displayed in the embed",
+)
+async def add_server(
+    interaction: discord.Interaction,
+    server_name: str,
+    ip_address: str,
+    port: int,
+    color: str | None,
+    icon_url: str | None,
+    image_url: str | None,
+):
+    await client.send_embed(
+        interaction, server_name, ip_address, port, color, icon_url, image_url
+    )
+    await interaction.response.send_message(
+        f"Successfully created {server_name}!", ephemeral=True
+    )
+
+import sys
+
+missing_envs = [e for e in ['BOT_TOKEN', 'CHANNEL_ID', 'GUILD_ID'] if not os.getenv(e)]
+if missing_envs:
+    print(f"Error: missing env vars {', '.join(missing_envs)}")
+    sys.exit(1)
+
+client.run(os.getenv('BOT_TOKEN'))
